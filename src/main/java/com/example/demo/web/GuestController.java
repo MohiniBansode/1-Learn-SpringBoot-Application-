1863099c3bcd12087ecd73f4faa8c7be62de43bf
Mohini Bansode <85739214+Mahii-bit@users.noreply.github.com>
2023-09-06 18:48:13 +0530

    first demo commit

---
+++ src/main/java/com/example/demo/web/GuestController.java
@@ -0,0 +1,24 @@
+package com.example.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+
+import com.example.demo.business.ReservationService;
+
+@Controller
+@RequestMapping("/guests")
+public class GuestController {
+    private final ReservationService reservationService;
+
+    public GuestController(ReservationService reservationService) {
+        this.reservationService = reservationService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String getGuests(Model model){
+        model.addAttribute("guests", this.reservationService.getHotelGuests());
+        return "hotel-guests";
+    }
+}
